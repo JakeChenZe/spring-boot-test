3f602f9a90fc11d1c84b93682f87f1a7a5368d2a
Jake <jake.chen@wingsingsupplychain.com>
2020-01-09 11:03:23 +0800

    init

---
+++ src/main/java/cn/zezai/SpringBootTestApplication.java
@@ -0,0 +1,36 @@
+/**
+ * WingSing CONFIDENTIAL
+ * _____________________
+ * 
+ * [2014] - [2015] WingSing Supply Chain Management Co. (Shenzhen) Ltd. 
+ * All Rights Reserved.
+ * 
+ * NOTICE: All information contained herein is, and remains the property of
+ * WingSing Supply Chain Management Co. (Shenzhen) Ltd. and its suppliers, if
+ * any. The intellectual and technical concepts contained herein are proprietary
+ * to WingSing Supply Chain Management Co. (Shenzhen) Ltd. and its suppliers and
+ * may be covered by China and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this information
+ * or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from WingSing Supply Chain Management Co. (Shenzhen)
+ * Ltd.
+ *
+ */
+
+package cn.zezai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+  * @Author created by Jake 
+  * @Date 2020年1月9日 上午10:13:35
+  */
+@SpringBootApplication
+public class SpringBootTestApplication {
+	
+	public static void main(String [] args){
+		SpringApplication.run(SpringBootTestApplication.class, args);
+	}
+
+}
